835d65ae298a515d8889376f84551177cc1ff1cb
Patrick Sørensen <hasfeldt@yahoo.dk>
2018-10-05 19:41:39 +0200

    Needs testing
    Needs additional testing.

--- vrp_outlawalert/client.lua
+++ vrp_outlawalert/client.lua
@@ -3,26 +3,45 @@ Tunnel.bindInterface("vRP_outlawalert",vRPol)
 Proxy.addInterface("vRP_outlawalert",vRPol)
 vRP = Proxy.getInterface("vRP")
 
---Config
+local blipList = {}
 
---[[local PedModels = {
-        "s_m_y_cop_01",
-        's_m_y_hwaycop_01',
-        's_f_y_cop_01',
-        's_m_y_sheriff_01',
-        's_f_y_sheriff_01',
-        's_m_m_ciasec_01',
-        'u_m_m_fibarchitect',
-        's_m_y_swat_01',
-    }
---]]
+RegisterNetEvent('advisor')
+AddEventHandler('advisor', function(x,y,z,message)
+    local ped = GetPlayerPed(PlayerPedId())
+    local blip = AddBlipForCoord(x+0.001,y+0.001,z+0.001)
+    SetBlipSprite(blip, 304)
+    SetBlipColour(blip, 67)
+    SetBlipAlpha(blip, 250)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Alarmcentral")
+    EndTextCommandSetBlipName(blip)
+
+    table.insert(blipList, blip)
+    
+    --Citizen.Trace("x:" .. x .. " y:" .. y .. " z:" .. z)
+    PlaySound(-1, "Menu_Accept", "Phone_SoundSet_Default", 0, 0, 1)
+    TriggerEvent('chatMessage', '^3[Alarmcentralen]', "^3[Alarmcentralen]", message)
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local ped = GetPlayerPed(PlayerPedId())
+        for k,v in pairs(blipList) do
+            RemoveBlip(v)
+        end
+        blipList = {}
+        Citizen.Wait(120000)
+    end
+end)
+
 
 Citizen.CreateThread( function()
     while true do
         Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        local pos = GetEntityCoords(GetPlayerPed(-1), true)
         if IsPedTryingToEnterALockedVehicle(GetPlayerPed(-1)) or IsPedJacking(GetPlayerPed(-1)) then
-                TriggerServerEvent('sendServiceAlrt', plyPos.x, plyPos.y, plyPos.z,theft)
+                TriggerServerEvent('dispatch', pos.x, pos.y,pos.z,theft)
                 Wait(5000)
             end
         end
@@ -32,9 +51,9 @@ end)
 Citizen.CreateThread( function()
     while true do
         Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        local pos = GetEntityCoords(GetPlayerPed(-1), true)
         if IsPedInMeleeCombat(GetPlayerPed(-1)) then 
-            TriggerServerEvent('sendServiceAlrt', plyPos.x, plyPos.y, plyPos.z,melee)
+            TriggerServerEvent('dispatch', pos.x, pos.y,pos.z,melee)
             Wait(3000)
         end
     end
@@ -43,9 +62,9 @@ end)
 Citizen.CreateThread( function()
     while true do
         Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        local pos = GetEntityCoords(GetPlayerPed(-1), true)
         if IsPedShooting(GetPlayerPed(-1)) then
-            TriggerServerEvent('sendServiceAlrt', plyPos.x, plyPos.y, plyPos.z,gunshot)
+            TriggerServerEvent('dispatch', pos.x, pos.y,pos.z,gunshot)
             Wait(3000)
         end
     end
--- vrp_outlawalert/server.lua
+++ vrp_outlawalert/server.lua
@@ -7,23 +7,43 @@ vRPclient = Tunnel.getInterface("vRP","vRP_cuffcutter")
 vRPol = Tunnel.getInterface("vRP_outlawalert","vRP_outlawalert")
 
 local randomname = {"Lisa","Frank","Peter","Bob","Christian","James","Michael","Maria","David","Mary","Robert","Joseph","John","Clark","Taylor","George","Henry"}
-local service_name = "police.service"
 local rgmsg = {"Help!! Someone is shooting!","I just heard gunfire!","Oh my god, someone is shooting! Please come fast","I JUST HEARD SHOTS, PLEASE COME FAST"}
 local rmmsg = {"There are people fighting here, help, quick!","Help! two people are going to beat eachother to death!","Two people fighting, come, quick!"}
 local rtmsg = {"Someone just stole a car!","I just witnessed theft of a vehicle, come, quick!","A guy just stole a car"}
 
+RegisterServerEvent('dispatch')
+AddEventHandler('dispatch', function(x,y,z,message,type)
+  local players = {}
+  local users = vRP.getUsers({})
+  local isPolice = false
+  local isEms = false
 
-RegisterServerEvent('sendServiceAlrt')
-AddEventHandler('sendServiceAlrt', function(x,y,z,type))
+    for k,v in pairs(users) do
+      
+        local player = vRP.getUserSource({k})
+        
+        if player ~= nil then
+          local user_id = vRP.getUserId({player})
+
+          isPolice = vRP.hasGroup({user_id,"police"})
+		  isEms = vRP.hasGroup({user_id,"ems"})
+          if isPolice or isEms then
+            table.insert(players,player)
+          end
+        end
+  end
+
+  for k,v in pairs(players) do
 	if type ~= nil then
-		if type == gunshot then
-			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rgmsg[math.random(#rgmsg)])
-		elseif type == melee then
-			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rmmsg[math.random(#rgmsg)])
+	 if type == melee then			
+			message = (randomname[math.random(#randomname)]), rtmsg[math.ranbdom(#rgmsg)])
+			TriggerClientEvent('advisor', v, x,y,z,message)
 		elseif type == theft then
-			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rtmsg[math.ranbdom(#rtmsg)])
+			message = (randomname[math.random(#randomname)]), rtmsg[math.ranbdom(#rtmsg)])
+			TriggerClientEvent('advisor', v, x,y,z,message)
 		end
-	else
+	 else
 		print("-- [DEBUG] -- TYPE = NIL")
-	end
+	 end
+    end
 end)
