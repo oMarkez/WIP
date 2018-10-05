fd758f63d79c4c85e24f2ff3c38ec583f2afc44d
DGVaniX <37979538+DGVaniX@users.noreply.github.com>
2018-10-05 18:47:43 +0100

    Update server.lua

--- vrp_outlawalert/server.lua
+++ vrp_outlawalert/server.lua
@@ -7,7 +7,7 @@ vRPclient = Tunnel.getInterface("vRP","vRP_cuffcutter")
 vRPol = Tunnel.getInterface("vRP_outlawalert","vRP_outlawalert")
 
 local randomname = {"Lisa","Frank","Peter","Bob","Christian","James","Michael","Maria","David","Mary","Robert","Joseph","John","Clark","Taylor","George","Henry"}
-local service_name = "police.service"
+local service_name = "police"
 local rgmsg = {"Help!! Someone is shooting!","I just heard gunfire!","Oh my god, someone is shooting! Please come fast","I JUST HEARD SHOTS, PLEASE COME FAST"}
 local rmmsg = {"There are people fighting here, help, quick!","Help! two people are going to beat eachother to death!","Two people fighting, come, quick!"}
 local rtmsg = {"Someone just stole a car!","I just witnessed theft of a vehicle, come, quick!","A guy just stole a car"}
@@ -16,12 +16,12 @@ local rtmsg = {"Someone just stole a car!","I just witnessed theft of a vehicle,
 RegisterServerEvent('sendServiceAlrt')
 AddEventHandler('sendServiceAlrt', function(x,y,z,type))
 	if type ~= nil then
-		if type == gunshot then
-			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rgmsg[math.random(#rgmsg)])
-		elseif type == melee then
-			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rmmsg[math.random(#rgmsg)])
-		elseif type == theft then
-			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rtmsg[math.ranbdom(#rtmsg)])
+		if type == "gunshot" then
+			vRP.sendServiceAlert(nil,service_name,x,y,z,randomname[math.random(#randomname)]..": "..rgmsg[math.random(#rgmsg)])
+		elseif type == "melee" then
+			vRP.sendServiceAlert(nil,service_name,x,y,z,randomname[math.random(#randomname)]..": "..rmmsg[math.random(#rmmsg)])
+		elseif type == "theft" then
+			vRP.sendServiceAlert(nil,service_name,x,y,z,randomname[math.random(#randomname)]..": "..rtmsg[math.ranbdom(#rtmsg)])
 		end
 	else
 		print("-- [DEBUG] -- TYPE = NIL")
