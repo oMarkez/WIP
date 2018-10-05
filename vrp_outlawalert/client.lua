0898e6d20fee2207e5b45a86bad5ec9b93edd330
oMarkez <40424824+oMarkez@users.noreply.github.com>
2018-10-05 18:31:45 +0200

    Add files via upload

---
+++ vrp_outlawalert/client.lua
@@ -0,0 +1,52 @@
+vRPcc = {}
+Tunnel.bindInterface("vRP_outlawalert",vRPol)
+Proxy.addInterface("vRP_outlawalert",vRPol)
+vRP = Proxy.getInterface("vRP")
+
+--Config
+
+--[[local PedModels = {
+        "s_m_y_cop_01",
+        's_m_y_hwaycop_01',
+        's_f_y_cop_01',
+        's_m_y_sheriff_01',
+        's_f_y_sheriff_01',
+        's_m_m_ciasec_01',
+        'u_m_m_fibarchitect',
+        's_m_y_swat_01',
+    }
+--]]
+
+Citizen.CreateThread( function()
+    while true do
+        Wait(0)
+        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        if IsPedTryingToEnterALockedVehicle(GetPlayerPed(-1)) or IsPedJacking(GetPlayerPed(-1)) then
+                TriggerServerEvent('sendServiceAlrt', plyPos.x, plyPos.y, plyPos.z,theft)
+                Wait(5000)
+            end
+        end
+    end
+end)
+
+Citizen.CreateThread( function()
+    while true do
+        Wait(0)
+        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        if IsPedInMeleeCombat(GetPlayerPed(-1)) then 
+            TriggerServerEvent('sendServiceAlrt', plyPos.x, plyPos.y, plyPos.z,melee)
+            Wait(3000)
+        end
+    end
+end)
+
+Citizen.CreateThread( function()
+    while true do
+        Wait(0)
+        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        if IsPedShooting(GetPlayerPed(-1)) then
+            TriggerServerEvent('sendServiceAlrt', plyPos.x, plyPos.y, plyPos.z,gunshot)
+            Wait(3000)
+        end
+    end
+end)
---
+++ vrp_outlawalert/server.lua
@@ -0,0 +1,29 @@
+--Settings--
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP","vRP_cuffcutter")
+vRPol = Tunnel.getInterface("vRP_outlawalert","vRP_outlawalert")
+
+local randomname = {"Lisa","Frank","Peter","Bob","Christian","James","Michael","Maria","David","Mary","Robert","Joseph","John","Clark","Taylor","George","Henry"}
+local service_name = police.service
+local rgmsg = {"Help!! Someone is shooting!","I just heard gunfire!","Oh my god, someone is shooting! Please come fast","I JUST HEARD SHOTS, PLEASE COME FAST"}
+local rmmsg = {"There are people fighting here, help, quick!","Help! two people are going to beat eachother to death!","Two people fighting, come, quick!"}
+local rtmsg = {"Someone just stole a car!","I just witnessed theft of a vehicle, come, quick!","A guy just stole a car"}
+
+
+RegisterServerEvent('sendServiceAlrt')
+AddEventHandler('sendServiceAlrt', function(x,y,z,type))
+	if type ~= nil then
+		if type == gunshot then
+			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rgmsg[math.random(#rgmsg)])
+		elseif type == melee then
+			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rmmsg[math.random(#rgmsg)])
+		elseif type == theft then
+			vRP.sendServiceAlert(randomname[math.random(#randomname)]), service_name,x,y,z,rtmsg[math.ranbdom(#rtmsg)])
+		end
+	else
+		print("-- [DEBUG] -- TYPE = NIL")
+	end
+end)
